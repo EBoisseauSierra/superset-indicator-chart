81ddb20e39999cf1d7a014f43d7dff27d2dd4200
Étienne Boisseau-Sierra <etienne.boisseau-sierra@unipart.io>
2022-03-07 11:14:34 +0000

    Hide Markdown component
    Signed-off-by: Étienne Boisseau-Sierra <etienne.boisseau-sierra@unipart.io>

--- src/buildQuery.ts
+++ src/buildQuery.ts
@@ -2,11 +2,12 @@ import { buildQueryContext } from '@superset-ui/core';
 import { FormData } from './types';
 
 export default function buildQuery(formData: FormData) {
-  // const { metrics } = formData;
+  const { groupby = [] } = formData;
 
   return buildQueryContext(formData, baseQueryObject => [
     {
       ...baseQueryObject,
+      columns: [...groupby],
     },
   ]);
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,9 @@
-import { AdhocFilter, QueryFormData, QueryFormMetric } from '@superset-ui/core';
+import {
+  AdhocFilter,
+  QueryFormColumn,
+  QueryFormData,
+  QueryFormMetric
+} from '@superset-ui/core';
 
 export type ChartProps = {
   markdowns: string[];
@@ -13,11 +18,10 @@ export type ChartProps = {
 export type FormData = QueryFormData & {
   adhoc_filters: AdhocFilter[];
   data_color_mapper: string;
-  groupby: string[];
+  groupby: QueryFormColumn[];
   markdown: string;
   metrics: QueryFormMetric[];
   number_format: string;
-  // order_desc: boolean;
   item_min_width: number;
   rounded_corners: boolean;
   text_color: string;
